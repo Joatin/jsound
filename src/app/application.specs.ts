fcbf872860355c80cc70047f616dc60ee800586b
Joatin Granlund <granlundjoatin@icloud.com>
2017-06-27 14:28:52 +0200

    ci(Travis): Added coverage

---
+++ src/app/application.specs.ts
@@ -0,0 +1,9 @@
+/* tslint:disable:only-arrow-functions */
+
+import { expect } from "chai";
+
+describe("Application", function() {
+  it("test", function() {
+    expect(true).to.equal(true);
+  });
+});
