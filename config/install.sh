76feb9c546c939a41abff08cd6ff94c36a576ea1
Joatin Granlund <granlundjoatin@icloud.com>
2017-06-28 14:21:39 +0200

    ci(travis): fix

--- config/install.sh
+++ config/install.sh
@@ -14,6 +14,4 @@ echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # Update the package list and install the Cloud SDK
-sudo apt-get install google-cloud-sdk
-
-gcloud components install kubectl
+sudo apt-get install google-cloud-sdk kubectl
