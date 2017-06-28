0eb685f0392fbbaf24908e95991106a341b6fd6e
Joatin Granlund <granlundjoatin@icloud.com>
2017-06-28 14:16:35 +0200

    ci(travis): test

--- config/install.sh
+++ config/install.sh
@@ -15,3 +15,5 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 # Update the package list and install the Cloud SDK
 sudo apt-get install google-cloud-sdk
+
+gcloud components install kubectl
